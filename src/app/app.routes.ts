381f5f961d001ed2ddee052ce8c97418624cb708
Mykola846 <stryzhakov356@gmail.com>
2025-10-18 21:21:19 +0300

    make new comp and configured routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },
+    { path: 'movies', component: MoviesComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', component: NotFoundComponent },
+];
